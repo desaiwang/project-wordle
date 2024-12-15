a29e6486d93a4c3d856b5808b8f797c6f0a0c6c6
Desai Wang <dw584@cornell.edu>
2024-12-15 14:49:19 +0800

    updated pattern check

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -3,24 +3,26 @@ import React from "react";
 function GuessInput() {
   const [guess, setGuess] = React.useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(guess);
+    setGuess("");
+  }
+
   return (
     <form
       className="guess-input-wrapper"
-      onSubmit={(event) => {
-        event.preventDefault();
-        console.log(guess.toUpperCase());
-        setGuess("");
-      }}
+      onSubmit={(event) => handleSubmit(event)}
     >
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
-        minLength={5}
-        pattern="\[a-zA-Z]{5,5}"
         required
+        pattern="[a-zA-Z]{5,5}"
+        title="5 letter word"
         value={guess}
-        onChange={(event) => setGuess(event.target.value)}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
       ></input>
     </form>
   );
